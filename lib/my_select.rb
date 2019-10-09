d74e0b8a9ca157b7d7523b807f9dd17c7ef8bd09
Mei Wong <fongmeiw@gmail.com>
2019-10-09 16:52:35 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,13 +1,15 @@
 def my_select(collection, selected)
-  i = 0
-  while i < collection.size do
-    yield(collection[i]) if block_given?
-    i += 1
-  end
+  if block_given?
+    i = 0
+    while i < collection.size do
+      yield(collection[i]) if block_given?
+      i += 1
+    end
+  end 
 end
 
-collection = [1,2,3]
-selected = 3
+collection = [1,2,3,4,5]
+
 my_select(collection) {|element|
   element.even?
 }
